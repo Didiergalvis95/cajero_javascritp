351b357a4ee82baf45913ed981648d12bbaad186
Didiergalvis95 <didiergalvis95@hotmail.com>
2022-11-10 16:12:51 -0500

    se arregla bug en opcion de inicio de sesion

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,35 +4,6 @@ const expresionValidarNumero = /^\d{4,15}$/;
 const expresionValidarCuenta = /^\d{11}$/;
 const expresionValidarCorreo = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 
-/* ---------------------- INICIAR SESION ---------------------- */
-const usuario = document.getElementById("usuario");
-const contraseña = document.getElementById("contraseña");
-const iniciarSesion = document.getElementById("iniciarSesion");
-const formulario = document.getElementById("formLoguin");
-const paginaError = document.getElementById("body_404");
-const textoAviso = document.getElementById("textoAviso");
-
-let errores = 0;
-iniciarSesion.addEventListener("click", (e) => {
-  if (usuario.value == "escrotoenmoto" && contraseña.value === "654321") {
-    usuario.classList.add("correcto");
-    usuario.classList.remove("incorrecto");
-    contraseña.classList.add("correcto");
-    contraseña.classList.remove("incorrecto");
-    console.log("el campo contraseña es obligatorio");
-  } else {
-    errores++;
-    usuario.classList.add("incorrecto");
-    contraseña.classList.add("incorrecto");
-    e.preventDefault();
-    if (errores == 3) {
-      formulario.classList.add("ocultar");
-      paginaError.classList.remove("ocultar");
-      textoAviso.classList.remove("ocultar");
-    }
-  }
-})
-
 
 /* ---------------------- CONFIRMACION CERRAR SESION ---------------------- */
 const btnCerrarSesion = document.querySelector('#cerrarSesion');
@@ -287,3 +258,32 @@ btnCerrarAlerta.addEventListener('click', ()=>{
     btnAlertaConsignar.classList.add('ocultar');
     opcionConsignar.classList.remove('ocultar');
 });
+
+/* ---------------------- INICIAR SESION ---------------------- */
+const usuario = document.getElementById("usuario");
+const contraseña = document.getElementById("contraseña");
+const iniciarSesion = document.getElementById("iniciarSesion");
+const formulario = document.getElementById("formLoguin");
+const paginaError = document.getElementById("body_404");
+const textoAviso = document.getElementById("textoAviso");
+
+let errores = 0;
+iniciarSesion.addEventListener("click", (e) => {
+  if (usuario.value == "escrotoenmoto" && contraseña.value === "654321") {
+    usuario.classList.add("correcto");
+    usuario.classList.remove("incorrecto");
+    contraseña.classList.add("correcto");
+    contraseña.classList.remove("incorrecto");
+    console.log("el campo contraseña es obligatorio");
+  } else {
+    errores++;
+    usuario.classList.add("incorrecto");
+    contraseña.classList.add("incorrecto");
+    e.preventDefault();
+    if (errores == 3) {
+      formulario.classList.add("ocultar");
+      paginaError.classList.remove("ocultar");
+      textoAviso.classList.remove("ocultar");
+    }
+  }
+})
