8f47bce818de2852bc20dbd0598f2d70584c9aa6
Didiergalvis95 <didiergalvis95@hotmail.com>
2022-11-10 15:35:58 -0500

    Se fusiona rama karen con rama master<2 q

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,4 +24,35 @@ iniciarSesion.addEventListener("click", (e) => {
       textoAviso.classList.remove("ocultar");
     }
   }
-});
+btnCerrarSesion.addEventListener('click',()=>{
+    const confirmarSalir= document.createElement ('input')
+    confirmarSalir.id='Salir'
+    confirmarSalir.type='button'
+    const nosalir=document.createElement('input')
+    nosalir.id='noSalir'
+    nosalir.type='button'
+    
+    const alertaCerrar=document.createElement ('h4')
+    alertaCerrar.id='alertaCerrar'
+
+    alertaCerrar.textContent='¿DESEA CERRAR SESION?'
+    
+    movimientos.classList.remove('mostrar')
+    movimientos.classList.add('ocultar');
+    mostrarSaldo.classList.add('ocultar');
+    confirmacion.classList.remove('ocultar');
+    
+    confirmarSalir.value='SI'
+    nosalir.value='NO'
+    confirmarSalir.addEventListener('click',()=>{
+        confirmacion.classList.add('ocultar');
+        window.location='./login.html'
+        
+    })
+    nosalir.addEventListener('click',()=>{
+        movimientos.classList.remove('ocultar');
+        confirmacion.classList.add('ocultar');
+        confirmacion.innerHTML = ''
+    })
+    confirmacion.append(alertaCerrar, confirmarSalir,nosalir)
+})})
