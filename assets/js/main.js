a673f3d8cf5058a0ecaf3d8700e377a50e715787
Didiergalvis95 <didiergalvis95@hotmail.com>
2022-11-10 15:07:48 -0500

    se crea pagina de login y estilos

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+const usuario = document.getElementById("usuario");
+const contraseña = document.getElementById("contraseña");
+const iniciarSesion = document.getElementById("iniciarSesion");
+const formulario = document.getElementById("formLoguin");
+const paginaError = document.getElementById("body_404");
+const textoAviso = document.getElementById("textoAviso");
+
+let errores = 0;
+iniciarSesion.addEventListener("click", (e) => {
+  if (usuario.value == "escrotoenmoto" && contraseña.value === "654321") {
+    usuario.classList.add("correcto");
+    usuario.classList.remove("incorrecto");
+    contraseña.classList.add("correcto");
+    contraseña.classList.remove("incorrecto");
+    console.log("el campo contraseña es obligatorio");
+  } else {
+    errores++;
+    usuario.classList.add("incorrecto");
+    contraseña.classList.add("incorrecto");
+    e.preventDefault();
+    if (errores == 3) {
+      formulario.classList.add("ocultar");
+      paginaError.classList.remove("ocultar");
+      textoAviso.classList.remove("ocultar");
+    }
+  }
+});
